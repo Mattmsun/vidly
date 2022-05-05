4b1497e6e5bee15fd3bc80ac9e1bcc2007b8fe35
Matt <mattmsun@gmail.com>
2022-05-05 15:12:39 +1000

    add suppot for connStr via an env variable

--- startup/logging.js
+++ startup/logging.js
@@ -3,19 +3,28 @@ const winston = require("winston");
 require("express-async-errors");
 
 module.exports = function () {
+  const logger = winston.createLogger({
+    level: "error",
+    transports: [
+      new winston.transports.File({ filename: "uncaughtException.log" }),
+    ],
+  });
+
   process.on("uncaughtException", (ex) => {
-    winston.info(ex.message, ex);
+    logger.error(ex.message);
+    winston.error(ex.message, ex);
     process.exit(1);
   });
 
-  winston.exceptions.handle(
-    new winston.transports.Console(),
-    new winston.transports.File({ filename: "uncaughtException.log" })
-  );
+  // winston.exceptions.handle(
+  //   new winston.transports.File({
+  //     filename: "uncaughtException.log",
+  //     handleExceptions: true,
+  //   })
+  // );
 
   process.on("unhandledRejection", (ex) => {
-    // winston.error(ex.message, ex);
-    // process.exit(1);
+    winston.error(ex.message, ex);
     throw ex;
   });
 
@@ -28,9 +37,9 @@ module.exports = function () {
       ),
     })
   );
-  winston.add(
-    new winston.transports.File({ filename: "logfile.log", level: "error" })
-  );
+  // winston.add(
+  //   new winston.transports.File({ filename: "logfile.log", level: "error" })
+  // );
   // winston.add(
   //   new winston.transports.MongoDB({
   //     db: "mongodb://localhost/vidly",
