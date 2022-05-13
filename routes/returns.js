2de174b936ff7b3e03b9a66c7266b89eb27342d3
Matt <mattmsun@gmail.com>
2022-05-14 00:41:55 +1000

    returns

--- routes/returns.js
+++ routes/returns.js
@@ -5,12 +5,33 @@ const { Rental } = require("../models/rental");
 const { Movie } = require("../models/movie");
 const Joi = require("joi");
 
-router.post("/", [auth], async (req, res) => {
-  if (!req.body.customerId)
-    return res.status(400).send("customerId is not provided");
-  if (!req.body.movieId) return res.status(400).send("movieId is not provided");
-  const rental = await Rental.lookup(req.body.customerId, req.body.movieId);
+// router.post("/", [auth], async (req, res) => {
+// if (!req.body.customerId)
+//   return res.status(400).send("customerId is not provided");
+//   if (!req.body.movieId) return res.status(400).send("movieId is not provided");
+//   const rental = await Rental.lookup(req.body.customerId, req.body.movieId);
 
+//   if (!rental) return res.status(404).send("Rental not found");
+
+//   if (rental.dateReturned)
+//     return res.status(400).send("Rental already processed");
+
+//   rental.return();
+//   await rental.save();
+//   //update
+//   await Movie.updateOne(
+//     { _id: rental.movie._id },
+//     {
+//       $inc: { numberInStock: 1 },
+//     }
+//   );
+//   return res.send(rental);
+// });
+
+router.post("/", [auth], async (req, res) => {
+  if (!req.body.rentalId)
+    return res.status(400).send("rentalId is not provided");
+  const rental = await Rental.findById(req.body.rentalId);
   if (!rental) return res.status(404).send("Rental not found");
 
   if (rental.dateReturned)
@@ -27,6 +48,7 @@ router.post("/", [auth], async (req, res) => {
   );
   return res.send(rental);
 });
+
 function validateReturn(req) {
   const schema = {
     customerId: Joi.objectId().required(),
